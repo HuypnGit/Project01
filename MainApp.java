458090776af33837ab6b11908882db9ce4ca99e0
unknown <huy.sirmaster@gmail.com>
2018-09-07 14:03:33 +0700

    tinh_tong

--- MainApp.java
+++ MainApp.java
@@ -1,10 +1,17 @@
 package huy.dev;
 
+import java.util.Scanner;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("nhap so nguyen duong : ");
+		int n= new Scanner(System.in).nextInt();
+		int Tong = 0;
+		for (int i=1;i<=n;i++) {
+			Tong = Tong + i;
+			}
+		System.out.println("Tong la "+Tong );
 	}
-
 }
+
